b9347dc20a2524c61ebfbef0bfc3686e0c187985
sebastian-j-ibanez <sebas.ibanez219@gmail.com>
2024-08-08 21:34:27 -0400

    Finish PacketizeFile. Implement chunkBuffer.

--- directory/directory.go
+++ directory/directory.go
@@ -3,6 +3,8 @@ package directory
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
+	prot "fsync/protocol"
 	"os"
 )
 
@@ -15,7 +17,63 @@ type FileHash struct {
 	Hash string
 }
 
-// Hash Directory
+// Convert file to packets
+func (d DirManager) PacketizeFile(name string) ([]prot.Packet, error) {
+	// Check if dirmanager has valid path
+	if err := d.checkPath(); err != nil {
+		return nil, err
+	}
+
+	// Read from file
+	fullPath := d.Path + "/" + name
+	fileData, err := os.ReadFile(fullPath)
+	if err != nil {
+		return nil, err
+	}
+ 	
+	// Calculate number of packets
+	pktNum := int(1)
+	fileSize := len(fileData)
+	if fileSize >= prot.MaxBodySize {
+		pktNum = int(fileSize) / int(prot.MaxBodySize)
+	}
+
+	// Convert file data to consistent-sized chunks 
+	data, err := chunkBuffer(fileData, prot.MaxBodySize)
+	if err != nil {
+		return nil, err
+	}
+	
+	// Create and append packets
+	var packets []prot.Packet
+	for i := range pktNum {
+		tmpPacket := prot.Packet{
+			OrderNum: i,
+			Body: data[i],
+		}
+		packets = append(packets, tmpPacket)
+	}
+	
+	return packets, nil
+}
+
+// Write data to file
+func (d DirManager) WriteDataToFile(name string, data []byte) error {
+	// Check if dirmanager has valid path
+	if err := d.checkPath(); err != nil {
+		return err
+	}
+
+	// Open file
+	fullPath := d.Path + "/" + name
+	if err := os.WriteFile(fullPath, data, 0666); err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// Hash files in directory
 func (d DirManager) HashDir() ([]FileHash, error) {
 	var hashes []FileHash
 	var err error
@@ -58,4 +116,25 @@ func (d DirManager) checkPath() error {
 		return err
 	}
 	return nil
+}
+
+// Convert byte slice into smaller evenly-sized byte slices (taking into account leftover any bytes)
+func chunkBuffer(buffer []byte, chunkSize int) ([][]byte, error) {
+	if chunkSize <= 0 {
+		return nil, errors.New("chunk size must be greater than 0")
+	}
+
+	var chunks [][]byte
+	dataLen := len(buffer)
+
+	// Iterate over the data and split it into chunks
+	for start := 0; start < dataLen; start += chunkSize {
+		end := start + chunkSize
+		if end > dataLen {
+			end = dataLen
+		}
+		chunks = append(chunks, buffer[start:end])
+	}
+	
+	return chunks, nil
 }
--- protocol/protocol.go
+++ protocol/protocol.go
@@ -2,19 +2,20 @@ package protocol
 
 import (
 	"encoding/gob"
-	"errors"
 	"net"
-	"os"
-	"fsync/util"
 )
 
-
-
-// SOCKET HANDLER
 type SocketHandler struct {
 	Peers []Peer
 	Con net.Conn
 }
+
+type Packet struct {
+	OrderNum int
+	Body []byte
+}
+
+const MaxBodySize = 61440
 
 func (s SocketHandler) SendPackets(packets []Packet) error {
 	enc := gob.NewEncoder(s.Con)
@@ -31,6 +32,12 @@ func (s SocketHandler) SendPackets(packets []Packet) error {
 	}
 	
 	return nil
+}
+
+func (s SocketHandler) SendPacket(packet Packet) error {
+	enc := gob.NewEncoder(s.Con)
+	err := enc.Encode(packet)
+	return err
 }
 
 func (s SocketHandler) ReceivePackets() ([]Packet, error) {
@@ -54,58 +61,10 @@ func (s SocketHandler) ReceivePackets() ([]Packet, error) {
 	return packets, nil
 }
 
-func (s SocketHandler) SendPacket(packet Packet) error {
-	enc := gob.NewEncoder(s.Con)
-	err := enc.Encode(packet)
-	return err
-}
-
-// PACKETS
-const kMaxBodySize = 61440
-
-type Packet struct {
-	OrderNum uint64
-	BodySize uint32
-	Body []byte
-}
-
-func PacketizeFile(path string, name string) ([]Packet, error) {
-	// Open file
-	fullPath := path + "/" + name
-	file, err := os.Open(fullPath)
-	util.CheckError(err)
-	defer file.Close()
-
-	// Get file size from stats
-	fstats, err := file.Stat()
-	util.CheckError(err)
-	fsize := fstats.Size()
-	if fsize <= 0 {
-		return nil, errors.New("cannot packetize empty file")
+func GetPacketData(packets []Packet) []byte {
+	var data []byte
+	for _, packet := range packets {
+		data = append(data, packet.Body...)
 	}
-	
-	// Calculate number of packets from file size
-	pktNum := int(1)
-	if fsize >= kMaxBodySize {
-		pktNum = int(fsize) / int(kMaxBodySize)
-	}
-	if (fsize % kMaxBodySize) > 0 {
-		pktNum++
-	}
-
-	// Create packets
-	var packets []Packet
-	offset := int64(0)
-	for i := range pktNum {
-		var tempBody []byte
-		bytesRead, err := file.ReadAt(tempBody, offset)
-		util.CheckError(err)
-		packets = append(packets, Packet{
-			uint64(i),
-			uint32(bytesRead),
-			tempBody,
-		})
-	}
-	
-	return packets, nil
+	return data
 }
