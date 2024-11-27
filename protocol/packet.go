3cb6f1a5165fce1cb98561e9ad8e81cb3444b30d
Your Name <you@example.com>
2024-11-27 07:43:44 -0500

    feat: add packet.go with packet type and helper functions

---
+++ protocol/packet.go
@@ -0,0 +1,48 @@
+package protocol
+
+import (
+	"bytes"
+	"encoding/gob"
+)
+
+type PacketType int
+
+const (
+	EncryptedPacket PacketType = iota
+	FileData
+	FileHashes
+	Int64
+)
+
+type Packet struct {
+	OrderNum int64
+	Body     []byte
+	Type     PacketType
+}
+
+// SerializeToBody data into packet body
+func (p *Packet) SerializeToBody(data interface{}, packetType PacketType) error {
+	p.Type = packetType
+
+	// Gob encode data
+	var dataBuf bytes.Buffer
+	enc := gob.NewEncoder(&dataBuf)
+	err := enc.Encode(data)
+	if err != nil {
+		return err
+	}
+
+	p.Body = dataBuf.Bytes()
+	return nil
+}
+
+// Deserialize packet body into data
+func (p *Packet) DeserializeBody(data interface{}) error {
+	dec := gob.NewDecoder(bytes.NewReader(p.Body))
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
