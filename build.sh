36d334aaf039e0c8a9d71a78ceb934aae5507b1d
Sebastian Ibanez <sebas.ibanez219@gmail.com>
2025-02-16 23:18:02 -0500

    build: print build time, create ~/.local/bin if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# October 7 2024
+# Sebastian J Ibanez
 # This script builds and copies the fsync binary to ~/.local/bin
 
 echo "[INFO] Building fsync binary..."
-go build
+build_time=$( { time go build; } 2>&1 )
+real_time=$( echo "$build_time" | grep real | awk '{print $2}' )
+
 echo "[INFO] Copying fsync binary to ~/.local/bin..."
+if ! [ -f ~/.local/bin ]; then
+    mkdir -p ~/.local/bin
+fi
 cp fsync ~/.local/bin
+
 echo "[INFO] Setup complete!"
+echo "[INFO] Build time: $real_time"
